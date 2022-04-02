1c0a834cd919311a76ea9ae63251932f5ae2eec4
Philipp Mukosey <philipp@mukosey.com>
2022-04-02 22:26:44 +0300

    lt - new fish alias for tree-like ls

---
+++ .config/fish/functions/lt.fish
@@ -0,0 +1,3 @@
+function lt --wraps='exa --color-scale -lT --git --git-ignore --group-directories-first --time-style iso' --description 'alias lt=exa --color-scale -lT --git --git-ignore --group-directories-first --time-style iso'
+  exa --color-scale -lT --git --git-ignore --group-directories-first --time-style iso $argv; 
+end
