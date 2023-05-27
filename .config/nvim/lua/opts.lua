8c84feb56859089d9bdf80538a755ed63803223f
Philipp Mukosey <philipp@mukosey.com>
2023-05-27 19:13:21 +0300

    NVIM: WSL clipboard fixed

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -1,16 +1,17 @@
 local o = vim.o -- Equal to :set
+local g = vim.g
 --local bo = vim.bo -- Equal to :setlocal
 --local go = vim.go -- Equal to :setglobal
 local cmd = vim.cmd
 
-
 -- external programs
-o.grepprg = 'rg -n ' -- Use ripgrep instead of grep
+if (vim.fn.systemlist('which grep')[1]) then
+    o.grepprg = 'rg -n ' -- Use ripgrep instead of grep
+end
 
 o.showmode = false
 -- cmd 'filetype plugin indent on'
 o.mouse = 'a' -- Enable mouse in all modes
-o.clipboard = 'unnamedplus' -- Use system`s clipboard instead of vim`s buffer
 o.number = true -- Show row numbers
 o.scrolloff = 5 -- Cursor is always 5 lines below and above the edge of the screen
 o.cursorline = true -- Highlight cursor line
@@ -22,12 +23,10 @@ o.ignorecase = true -- Case-insensitive search
 o.incsearch = true -- Search before 'Enter' hit
 o.smartcase = true -- Search is case-insensitive only if search string is all lowercase
 o.wrapscan = true -- Make search cyclic
--- --
-
 
 --vim.g.gruvbox_contrast_dark = 'soft'
 vim.opt.termguicolors = true
-vim.o.guifont = "Iosevka:h16"
+vim.o.guifont = 'Iosevka:h16'
 o.background = 'dark'
 
 -- encoding
@@ -58,11 +57,26 @@ o.undofile = true
 -- tabs and spaces view
 -- o.list = true
 -- o.listchars
---
-
 
 -- keyboard layouts
 cmd 'setlocal spell spelllang=ru_yo,en_us'
 o.keymap = 'russian-jcukenwin'
 o.iminsert = 0
 o.imsearch = 0
+
+-- clipboard
+o.clipboard = 'unnamedplus' -- Use system`s clipboard instead of vim`s buffer
+if (vim.fn.systemlist 'uname -a'[1]:find'icrosoft') then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
