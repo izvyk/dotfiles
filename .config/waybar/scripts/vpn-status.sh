f6793f9a912222e489e1a692b9313fc8d4ae8561
Philipp Mukosey <philipp@mukosey.com>
2024-05-09 11:19:35 +0200

    VPN-related scripts updated

--- .config/waybar/scripts/vpn-status.sh
+++ .config/waybar/scripts/vpn-status.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-if $(nmcli con show --active | grep wireguard -q); then
-	ICON="󱙱"
-	CLASS="disabled"
-else
+if $(nmcli con show --active | grep -e "vpn\|wireguard" -q); then
 	ICON="󰌾"
 	CLASS="enabled"
+else
+	ICON="󱙱"
+	CLASS="disabled"
 fi
 printf '{"text": "%s", "class": "%s"}\n' "$ICON" "$CLASS"
--- .config/waybar/scripts/vpn-toggle.sh
+++ .config/waybar/scripts/vpn-toggle.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-CONNECTION_NAME="$(nmcli con show | grep wireguard | cut -d' ' -f1)"
+CONNECTION_NAME="$(nmcli -f NAME,TIMESTAMP,TYPE con show | grep -e 'vpn\|wireguard' | sort -nk 2 | tail -n 1 | cut -d ' ' -f 1)"
 
-if $(nmcli con show --active | grep wireguard -q); then
+#if $(nmcli con show --active | grep wireguard -q); then
+if [[ -n "$(nmcli -g GENERAL.STATE c s ${CONNECTION_NAME})" ]]; then
     nmcli con down "$CONNECTION_NAME"
 else
     nmcli con up "$CONNECTION_NAME"
