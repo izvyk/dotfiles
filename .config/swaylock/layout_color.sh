99f0c38c1e8b662eb6f931004a15e1f99d471fa9
Philipp Mukosey <philipp@mukosey.com>
2024-05-10 13:50:38 +0200

    Swaylock layout related stuff removed

--- .config/swaylock/layout_color.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-grayscale=$(/usr/bin/grim -g "445,590 395x80" - | /usr/bin/convert - -resize 1x1\! -colorspace Gray -format "%[fx:int(255*c)]" info:-)
-if (( "$grayscale" > 127 ))
-then
-	echo 1d2021
-	exit
-fi
-
-echo ebdbb2
