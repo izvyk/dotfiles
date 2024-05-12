1f168848f89b4d2e2773de926d410269dd753bd0
Philipp Mukosey <philipp@mukosey.com>
2024-05-12 21:36:45 +0200

    Waybar config updated accordingly

--- .config/waybar/scripts/coffee-status.sh
+++ .config/waybar/scripts/coffee-status.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-if [[ "$(pidof swayidle)" ]]; then
-	ICON="󰾪"
-	CLASS="disabled"
-else
+if $(systemctl --user is-active inhibitor-lock@coffee.service -q); then
 	ICON="󰅶"
 	CLASS="enabled"
+else
+	ICON="󰾪"
+	CLASS="disabled"
 fi
 printf '{"text": "%s", "class": "%s"}\n' "$ICON" "$CLASS"
