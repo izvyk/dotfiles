0af299cd514ad87f2fc305c8970fca6a5a3bce2f
Philipp Mukosey <philipp@mukosey.com>
2024-05-12 04:05:04 +0200

    Swaylock removed and Hyprland config modified

--- .local/bin/lock-is-private.sh
+++ .local/bin/lock-is-private.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 
-current_workspace_id="$(hyprctl activeworkspace -j | jq -r '.id')"
+current_workspace_id="$(hyprctl activeworkspace -j | gojq -r '.id')"
 clients="$(hyprctl -j clients)"
-current_workspace_clients="$(echo "$clients" | jq -r ".[] | select(.workspace.id == $current_workspace_id)")"
+current_workspace_clients="$(echo "$clients" | gojq -r ".[] | select(.workspace.id == $current_workspace_id)")"
 
-if $(echo "$current_workspace_clients" | jq -r "select(.class == \"firefox\").title" | grep -iq 'Firefox Private Browsing'); then
+if $(echo "$current_workspace_clients" | gojq -r "select(.class == \"firefox\").title" | grep -iq 'Firefox Private Browsing'); then
 	exit 1
 fi
 
-if [[ -n "$(echo "$current_workspace_clients" | jq -r "select(.initialTitle == \"Picture-in-Picture\")")" ]]; then
-	if $(echo "$clients" | jq -r ".[] | select(.class == \"firefox\").title" | grep -iq 'Firefox Private Browsing'); then
+if [[ -n "$(echo "$current_workspace_clients" | gojq -r "select(.initialTitle == \"Picture-in-Picture\")")" ]]; then
+	if $(echo "$clients" | gojq -r ".[] | select(.class == \"firefox\").title" | grep -iq 'Firefox Private Browsing'); then
 		exit 1
 	fi
 fi
 
-if $(echo "$current_workspace_clients" | jq -r ".class" | grep -iq keepassxc); then
+if $(echo "$current_workspace_clients" | gojq -r ".class" | grep -iq keepassxc); then
 	exit 1
 fi
 
