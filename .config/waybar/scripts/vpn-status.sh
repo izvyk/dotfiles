ad8edea4b36319299dae3f4eaecc53f39724033a
Philipp Mukosey <philipp@mukosey.com>
2024-05-20 01:57:22 +0200

    Waybar config update

--- .config/waybar/scripts/vpn-status.sh
+++ .config/waybar/scripts/vpn-status.sh
@@ -9,5 +9,6 @@ else
 fi
 
 CONNECTION_NAME="$(nmcli con show --active | grep -e 'vpn\|wireguard' | cut -d' ' -f1)"
+if [ -n "$CONNECTION_NAME" ]; then CONNECTION_NAME="Name: $CONNECTION_NAME"; fi
 
 printf '{"text": "%s", "class": "%s", "tooltip": "%s"}\n' "$ICON" "$CLASS" "$CONNECTION_NAME"
