d3870faa0c1c8159a5dc7a01fed6c4ec472ab317
Philipp Mukosey <philipp@mukosey.com>
2024-05-21 20:09:05 +0200

    Hyprlock 'now playing' max length limitation & config cleanup

--- .local/bin/currentsong.sh
+++ .local/bin/currentsong.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/sh
 
 #echo "$(playerctl metadata --format '{{title}} 󰎆  {{artist}}')"
-echo "$(playerctl -s metadata --format '{{title}} 󰽱  {{artist}}')"
+playing="$(playerctl -s metadata --format '{{title}} 󰽱  {{artist}}')"
+maxlength="$1"
+
+if [ "$maxlength" == "" ] || [ "${#playing}" -lt "$maxlength" ]; then
+    echo "$playing";
+else
+    echo "${playing:0:$maxlength}...";
+fi
