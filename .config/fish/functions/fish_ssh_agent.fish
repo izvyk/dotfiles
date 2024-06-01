19804a8ccf6d01353632b94cbe0c81a3a9c5deed
Philipp Mukosey <philipp@mukosey.com>
2024-06-01 20:57:39 +0200

    KeePassXC ssh-agent integration added

--- .config/fish/functions/fish_ssh_agent.fish
+++ .config/fish/functions/fish_ssh_agent.fish
@@ -1,34 +1,6 @@
-# https://gitlab.com/kyb/fish_ssh_agent
-
-function __ssh_agent_is_started -d "check if ssh agent is already started"
-   if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
-      source $SSH_ENV > /dev/null
-   end
-
-   if test -z "$SSH_AGENT_PID"
-      return 1
-   end
-
-   ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep -q ssh-agent
-   #pgrep ssh-agent
-   return $status
-end
-
-
-function __ssh_agent_start -d "start a new ssh agent"
-   ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-   chmod 600 $SSH_ENV
-   source $SSH_ENV > /dev/null
-   true  # suppress errors from setenv, i.e. set -gx
-end
-
-
-function fish_ssh_agent --description "Start ssh-agent if not started yet, or uses already started ssh-agent."
-   if test -z "$SSH_ENV"
-      set -xg SSH_ENV $HOME/.ssh/environment
-   end
-
-   if not __ssh_agent_is_started
-      __ssh_agent_start
-   end
+function fish_ssh_agent --description "Start ssh-agent if it's not running"
+    if not pidof ssh-agent >/dev/null
+        eval "$(ssh-agent -c | head -2)"
+        echo STARTING SSH AGENT
+    end
 end
