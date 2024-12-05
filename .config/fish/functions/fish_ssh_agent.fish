2885a792321b78c35eee2ce09f51ead02ad91168
Philipp Mukosey <philipp@mukosey.com>
2024-12-05 10:45:57 +0100

    Fix fish ssh agent

--- .config/fish/functions/fish_ssh_agent.fish
+++ .config/fish/functions/fish_ssh_agent.fish
@@ -1,5 +1,5 @@
 function fish_ssh_agent --description "Start ssh-agent if it's not running"
-    if not pidof ssh-agent >/dev/null
-        eval (ssh-agent -c) > /dev/null
+    if not pidof ssh-agent > /dev/null || [ "$(pidof ssh-agentt)" != "$SSH_AGENT_PID" ]
+        eval $(ssh-agent -c) > /dev/null
     end
 end
