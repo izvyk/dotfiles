a298514c81d254e93aa893ea079ef938fae804ed
Philipp Mukosey <philipp@mukosey.com>
2026-02-28 00:00:59 +0100

    Remove old neovim configs

--- dot_config/nvim/lua/config/nvim-treesitter.lua
+++
@@ -1,9 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-    -- Modules and its options go here
-    highlight = {
-        enable = true,
-        disable = { 'cpp', 'lua', 'tex' }
-    },
-    incremental_selection = { enable = true },
-    textobjects = { enable = true },
-}
